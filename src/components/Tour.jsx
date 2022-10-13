a494ce661e942998d1e69c4a8738f342a00b2f05
MyNameIsDevid <hdevid12@gmail.com>
2022-10-13 12:42:24 +0200

    okt13

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,39 +1,78 @@
-import React,{useState,useEffect} from 'react';
+import React, { useEffect, useState } from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-export const Tour=({image,title,info,price})=> {
-    const [infoPartial,setInfoPartial] = useState('')
-
-    useEffect(() => {
-        const text50 = info.slice(0,50)
-        const lastSpacePos = text50.lastIndexOf(' ')
-        setInfoPartial(text50.slice(0,lastSpacePos))
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+ 
+ 
+ 
+ 
+const style = {
+    position: 'absolute' ,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 300,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+ 
+ 
+ 
+ 
+export const Tour=({image,info,name,price})=> {
+    const [infoPartila,setinfoPartial]=useState('')
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+ 
+ 
+    useEffect(()=>{
+        const text50=info.slice(0,50)
+        const lastSpacePos=text50.lastIndexOf(' ')
+        setinfoPartial(text50.slice(0,lastSpacePos))
     },[])
-
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
         image={image}
-        alt={title}
+        alt={name}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {title}
+          {name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {infoPartial}...
-          <Button size="small">Read More</Button>
+          {infoPartila}...
+          <Button onClick={handleOpen} size="small">Read More</Button>
+          <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {name}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {info}
+          </Typography>
+        </Box>
+      </Modal>
         </Typography>
       </CardContent>
       <CardActions>
-        <p size="small">Price: ${price}</p>
+        <p size="small">Price: {price}$</p>
+ 
       </CardActions>
     </Card>
   )
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import {useQuery} from 'react-query';
+import React from 'react'
+import {useQuery } from 'react-query';
 import axios from 'axios';
 import {Tour} from './Tour';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
+import { Typography } from '@mui/material';
+ 
 const getData=async ()=>{
-    const response = await axios.get('https://course-api.com/react-tours-project')
+    const response=await axios.get('https://course-api.com/react-tours-project')
     return await response
 }
-
+ 
 export const Tours=()=> {
-    const {data,status} = useQuery('tours',getData)
-    //status=='success' && console.log(typeof(data),Object.keys(data),data.data)
+    const {data,status}=useQuery('tours',getData)
     status=='success' && console.log(data.data)
-
-
+ 
   return (
     <Box sx={{ width: '100%' }}>
-      <Typography variant="h3" gutterBottom display="flex" justifyContent="center" sx={{textDecoration:'underline',color:'#1976d2'}}>
+      <Typography gutterBottom display="flex" justifyContent="center" variant="h3" sx={{textDecoration:"underline",color:"#1976d2"}}>
         Our Tours
       </Typography>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {status=='error' && <div>error fetching data...</div>}
-        {status=='loading' && <div>loading...</div>}
+        {status=='loading' && <div>loading</div>}
         {status=='success' && data.data.map(obj=>(
-           <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
-            <Tour key={obj.id} {...obj}/>
-            </Grid>
-            ))}
+        <Grid item xs={12} sm={6} md={3} display="flex" justifyContent="center">
+        <Tour key={obj.id} {...obj}/>
+        </Grid>
+        ))}
       </Grid>
+ 
     </Box>
   )
 }
